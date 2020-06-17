d44eeadec9910f61ea7d6976003a5767b4824606
tsorak <44933179+tsorak@users.noreply.github.com>
2020-06-17 09:26:41 +0200

    Add project

---
+++ resmonV2/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace resmonV2
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.lbl_debug = new System.Windows.Forms.Label();
+            this.tbx_pName = new System.Windows.Forms.TextBox();
+            this.lbx_matches = new System.Windows.Forms.ListBox();
+            this.btn_suspend = new System.Windows.Forms.Button();
+            this.btn_resume = new System.Windows.Forms.Button();
+            this.btn_terminate = new System.Windows.Forms.Button();
+            this.cbx_showAll = new System.Windows.Forms.CheckBox();
+            this.lbl_selectedProc = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbl_debug
+            // 
+            this.lbl_debug.AutoSize = true;
+            this.lbl_debug.Location = new System.Drawing.Point(497, 280);
+            this.lbl_debug.Name = "lbl_debug";
+            this.lbl_debug.Size = new System.Drawing.Size(37, 13);
+            this.lbl_debug.TabIndex = 0;
+            this.lbl_debug.Text = "debug";
+            // 
+            // tbx_pName
+            // 
+            this.tbx_pName.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbx_pName.Location = new System.Drawing.Point(12, 12);
+            this.tbx_pName.Name = "tbx_pName";
+            this.tbx_pName.Size = new System.Drawing.Size(477, 31);
+            this.tbx_pName.TabIndex = 1;
+            // 
+            // lbx_matches
+            // 
+            this.lbx_matches.Enabled = false;
+            this.lbx_matches.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lbx_matches.FormattingEnabled = true;
+            this.lbx_matches.ItemHeight = 16;
+            this.lbx_matches.Location = new System.Drawing.Point(12, 49);
+            this.lbx_matches.Name = "lbx_matches";
+            this.lbx_matches.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.lbx_matches.Size = new System.Drawing.Size(477, 260);
+            this.lbx_matches.TabIndex = 2;
+            this.lbx_matches.SelectedIndexChanged += new System.EventHandler(this.Lbx_matches_SelectedIndexChanged);
+            // 
+            // btn_suspend
+            // 
+            this.btn_suspend.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.btn_suspend.Location = new System.Drawing.Point(500, 49);
+            this.btn_suspend.Name = "btn_suspend";
+            this.btn_suspend.Size = new System.Drawing.Size(167, 37);
+            this.btn_suspend.TabIndex = 3;
+            this.btn_suspend.Text = "Suspend";
+            this.btn_suspend.UseVisualStyleBackColor = true;
+            this.btn_suspend.Click += new System.EventHandler(this.Btn_suspend_Click);
+            // 
+            // btn_resume
+            // 
+            this.btn_resume.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.btn_resume.Location = new System.Drawing.Point(500, 92);
+            this.btn_resume.Name = "btn_resume";
+            this.btn_resume.Size = new System.Drawing.Size(167, 37);
+            this.btn_resume.TabIndex = 4;
+            this.btn_resume.Text = "Resume";
+            this.btn_resume.UseVisualStyleBackColor = true;
+            this.btn_resume.Click += new System.EventHandler(this.Btn_resume_Click);
+            // 
+            // btn_terminate
+            // 
+            this.btn_terminate.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.btn_terminate.Location = new System.Drawing.Point(500, 135);
+            this.btn_terminate.Name = "btn_terminate";
+            this.btn_terminate.Size = new System.Drawing.Size(167, 37);
+            this.btn_terminate.TabIndex = 5;
+            this.btn_terminate.Text = "Terminate";
+            this.btn_terminate.UseVisualStyleBackColor = true;
+            this.btn_terminate.Click += new System.EventHandler(this.Btn_terminate_Click);
+            // 
+            // cbx_showAll
+            // 
+            this.cbx_showAll.AutoSize = true;
+            this.cbx_showAll.Location = new System.Drawing.Point(500, 296);
+            this.cbx_showAll.Name = "cbx_showAll";
+            this.cbx_showAll.Size = new System.Drawing.Size(119, 17);
+            this.cbx_showAll.TabIndex = 6;
+            this.cbx_showAll.Text = "Show All Processes";
+            this.cbx_showAll.UseVisualStyleBackColor = true;
+            this.cbx_showAll.CheckedChanged += new System.EventHandler(this.Cbx_showAll_CheckedChanged);
+            // 
+            // lbl_selectedProc
+            // 
+            this.lbl_selectedProc.AutoSize = true;
+            this.lbl_selectedProc.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.lbl_selectedProc.Location = new System.Drawing.Point(497, 12);
+            this.lbl_selectedProc.Name = "lbl_selectedProc";
+            this.lbl_selectedProc.Size = new System.Drawing.Size(157, 24);
+            this.lbl_selectedProc.TabIndex = 7;
+            this.lbl_selectedProc.Text = "Selected Process";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(680, 324);
+            this.Controls.Add(this.lbl_selectedProc);
+            this.Controls.Add(this.cbx_showAll);
+            this.Controls.Add(this.btn_terminate);
+            this.Controls.Add(this.btn_resume);
+            this.Controls.Add(this.btn_suspend);
+            this.Controls.Add(this.lbx_matches);
+            this.Controls.Add(this.tbx_pName);
+            this.Controls.Add(this.lbl_debug);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmMain";
+            this.Text = "Resource Monitor V2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_debug;
+        private System.Windows.Forms.TextBox tbx_pName;
+        private System.Windows.Forms.ListBox lbx_matches;
+        private System.Windows.Forms.Button btn_suspend;
+        private System.Windows.Forms.Button btn_resume;
+        private System.Windows.Forms.Button btn_terminate;
+        private System.Windows.Forms.CheckBox cbx_showAll;
+        private System.Windows.Forms.Label lbl_selectedProc;
+    }
+}
+
---
+++ resmonV2/Form1.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Windows.Input;
+
+namespace resmonV2
+{
+    public partial class frmMain : Form
+    {
+        Process[] processes;
+        Process selectedProc;
+        List<int> selectedProcs = new List<int>();
+
+        public frmMain()
+        {
+            InitializeComponent();
+            //SuspendProcess(1032);
+            //ResumeProcess(1032);
+            //getPidByName("CAM.DES");
+            tbx_pName.TextChanged += tbx_pName_TextChanged;
+        }
+
+        private void tbx_pName_TextChanged(object sender, EventArgs e)
+        {
+            getPidByName(tbx_pName.Text);
+        }
+
+        [Flags]
+        public enum ThreadAccess : int
+        {
+            TERMINATE = (0x0001),
+            SUSPEND_RESUME = (0x0002),
+            GET_CONTEXT = (0x0008),
+            SET_CONTEXT = (0x0010),
+            SET_INFORMATION = (0x0020),
+            QUERY_INFORMATION = (0x0040),
+            SET_THREAD_TOKEN = (0x0080),
+            IMPERSONATE = (0x0100),
+            DIRECT_IMPERSONATION = (0x0200)
+        }
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr OpenThread(ThreadAccess dwDesiredAccess, bool bInheritHandle, uint dwThreadId);
+        [DllImport("kernel32.dll")]
+        static extern uint SuspendThread(IntPtr hThread);
+        [DllImport("kernel32.dll")]
+        static extern int ResumeThread(IntPtr hThread);
+        [DllImport("kernel32", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern bool CloseHandle(IntPtr handle);
+
+        public void printProcess(Process[] processes)
+        {
+            lbx_matches.Items.Clear();
+            foreach (var process in processes)
+            {
+                lbx_matches.Items.Add(process.ProcessName.ToString() + "(" + process.Id.ToString() + ")" + " responding: " + process.Responding.ToString());
+            }
+        }
+
+        public void getPidByName(string pName)
+        {
+            lbx_matches.Items.Clear();
+            processes = Process.GetProcessesByName(pName);
+            lbl_debug.Text = processes.Length.ToString();
+            printProcess(processes);
+        }
+        public static void SuspendProcess(int pid)
+        {
+            var process = Process.GetProcessById(pid); // throws exception if process does not exist
+
+            foreach (ProcessThread pT in process.Threads)
+            {
+                IntPtr pOpenThread = OpenThread(ThreadAccess.SUSPEND_RESUME, false, (uint)pT.Id);
+
+                if (pOpenThread == IntPtr.Zero)
+                {
+                    continue;
+                }
+
+                SuspendThread(pOpenThread);
+
+                CloseHandle(pOpenThread);
+            }
+        }
+
+        public static void ResumeProcess(int pid)
+        {
+            var process = Process.GetProcessById(pid);
+
+            if (process.ProcessName == string.Empty)
+                return;
+
+            foreach (ProcessThread pT in process.Threads)
+            {
+                IntPtr pOpenThread = OpenThread(ThreadAccess.SUSPEND_RESUME, false, (uint)pT.Id);
+
+                if (pOpenThread == IntPtr.Zero)
+                {
+                    continue;
+                }
+
+                var suspendCount = 0;
+                do
+                {
+                    suspendCount = ResumeThread(pOpenThread);
+                } while (suspendCount > 0);
+
+                CloseHandle(pOpenThread);
+            }
+        }
+
+        private void Btn_suspend_Click(object sender, EventArgs e)
+        {
+            if (processes.Length > 0 && tbx_pName.Enabled)
+            {
+                foreach (var process in processes)
+                {
+                    SuspendProcess(process.Id);
+                }
+                printProcess(processes);
+            }
+            else if (selectedProcs.Count > 0)
+            {
+                foreach (var proc in selectedProcs)
+                {
+                    Console.WriteLine("Suspending " + processes.ElementAt(proc).ProcessName + "(" + processes.ElementAt(proc).Id + ")");
+                    SuspendProcess(processes.ElementAt(proc).Id);
+                }
+                selectedProcs.Clear();
+                lbl_selectedProc.Text = "";
+                updateProcesses();
+            }
+            else
+            {
+                lbl_debug.Text = "Suspending " + selectedProc.ProcessName;
+                SuspendProcess(selectedProc.Id);
+                lbl_debug.Text = "Suspended " + selectedProc.ProcessName;
+                updateProcesses();
+            }
+        }
+
+        private void Btn_resume_Click(object sender, EventArgs e)
+        {
+            //tbx_pName.Text = "BRUH";
+            if (processes.Length > 0 && tbx_pName.Enabled)
+            {
+                foreach (var process in processes)
+                {
+                    ResumeProcess(process.Id);
+                }
+                printProcess(processes);
+            }
+            else if (selectedProcs.Count > 0)
+            {
+                foreach (var proc in selectedProcs)
+                {
+                    Console.WriteLine("Resuming " + processes.ElementAt(proc).ProcessName + "(" + processes.ElementAt(proc).Id + ")");
+                    ResumeProcess(processes.ElementAt(proc).Id);
+                }
+                selectedProcs.Clear();
+                lbl_selectedProc.Text = "";
+                updateProcesses();
+            }
+            else
+            {
+                lbl_debug.Text = "Resuming " + selectedProc.ProcessName;
+                ResumeProcess(selectedProc.Id);
+                lbl_debug.Text = "Resumed " + selectedProc.ProcessName;
+                updateProcesses();
+            }
+        }
+        private void Btn_terminate_Click(object sender, EventArgs e)
+        {
+            //tbx_pName.Text = "BRUH";
+            if (processes.Length > 0 && tbx_pName.Enabled)
+            {
+                foreach (var process in processes)
+                {
+                    process.Kill();
+                    processes = processes.Where(proc => process.Responding != false).ToArray();
+                    printProcess(processes);
+                    lbl_debug.Text = processes.Length.ToString();
+                }
+            }
+            else if(selectedProcs.Count > 0)
+            {
+                foreach(var proc in selectedProcs)
+                {
+                    Console.WriteLine("Terminating " + processes.ElementAt(proc).ProcessName + "(" + processes.ElementAt(proc).Id + ")");
+                    processes.ElementAt(proc).Kill();
+                }
+                selectedProcs.Clear();
+                lbl_selectedProc.Text = "";
+                updateProcesses();
+            }
+            else
+            {
+                lbl_debug.Text = "Terminating " + selectedProc.ProcessName;
+                selectedProc.Kill();
+                lbl_debug.Text = "Terminated " + selectedProc.ProcessName;
+                updateProcesses();
+            }
+        }
+
+        private static Process[] sortProcesses(Process[] unSortedProcesses)
+        {
+            Process[] sortedProcesses = unSortedProcesses;
+            Array.Sort(sortedProcesses, delegate (Process proc1, Process proc2) { return proc1.ProcessName.CompareTo(proc2.ProcessName); });
+            return sortedProcesses;
+        }
+        private void updateProcesses()
+        {
+            processes = sortProcesses(Process.GetProcesses());
+            printProcess(processes);
+        }
+
+        private void Cbx_showAll_CheckedChanged(object sender, EventArgs e)
+        {
+            Process[] clear = { };
+            if (cbx_showAll.Checked)
+            {
+                tbx_pName.Enabled = false;
+                lbx_matches.Enabled = true;
+                lbx_matches.Focus();
+                processes = clear;
+                processes = sortProcesses(Process.GetProcesses());
+                printProcess(processes);
+                lbl_debug.Text = processes.Length.ToString();
+            }
+            else
+            {
+                tbx_pName.Enabled = true;
+                lbx_matches.Enabled = false;
+                processes = clear;
+                printProcess(processes);
+                lbl_debug.Text = processes.Length.ToString();
+            }
+        }
+
+        private void Lbx_matches_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //selectedProc = processes.ElementAt(lbx_matches.SelectedIndex);
+            
+            //lbl_debug.Text = selectedProc.Id.ToString();
+            if(lbx_matches.SelectedItems.Count > 1)
+            {
+                lbl_selectedProc.Text = "";
+                selectedProcs.Clear();
+                foreach (var item in lbx_matches.SelectedItems)
+                {
+                    lbl_selectedProc.Text += " " + lbx_matches.Items.IndexOf(item).ToString();
+                    //selectedProcs = processes.Where(proc => proc.Id == lbx_matches.Items.IndexOf(item)).ToArray();
+                    selectedProcs.Add(lbx_matches.Items.IndexOf(item));
+                    Console.WriteLine(lbx_matches.Items.IndexOf(item).ToString());
+                }
+                //processes = selectedProcs;
+            }
+            else if(lbx_matches.SelectedItems.Count > 0)
+            {
+                lbl_selectedProc.Text = processes.ElementAt(lbx_matches.SelectedIndex).Id.ToString();
+                selectedProc = processes.ElementAt(lbx_matches.SelectedIndex);
+            }
+        }
+    }
+}
